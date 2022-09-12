e1f5b1d13518fd930059d921dc68829c8597899d
sochii-i <princesschimaobi79@gmail.com>
2022-09-12 07:55:22 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 # include <stdio.h>
 
 /**
-* Main - print the string in the put function
-*Description: using the main function
-* this program prints "programming is like building a multilingual puzzle
+* main - print the string in the 
+* Description: using the main function 
+* this program prints "programming is like building a multilingual puzzle 
 * Return: o
 */
 Int main(void)
 {
-Puts (")"programming is like building a multilingual puzzle");
-return (o):
+          Put (")"programming is like building a multilingual puzzle ");
+          Return (o):
 }
